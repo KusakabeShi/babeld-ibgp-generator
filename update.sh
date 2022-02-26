ff2ec4a5b5ceadd371cfc7cdd0262ef5332182d9
root <root@example.com>
2022-02-27 00:17:32 +0800

    fix ipv6

--- update.sh
+++ update.sh
@@ -5,7 +5,20 @@ resolveip()
   : ${1:?Usage: resolve name}
   (
     PATH=$PATH:/usr/bin
-    getent hosts $1 | awk '{print $1}'
+    lookupresult=$(getent ahostsv4 "$1")
+    if [ $? -eq 0 ]; then
+        resultaddr=$(echo $lookupresult | head -n 1 | awk '{print $1}')
+        echo $resultaddr
+        return 0
+    fi
+    lookupresult=$(getent ahostsv6 $1)
+    if [ $? -eq 0 ]; then
+        resultaddr=$(echo $lookupresult | head -n 1 | awk '{print $1}')
+        echo "[$resultaddr]"
+        return 0
+    fi
+    echo "0.0.0.0"
+    return 127
   )
 }
 
